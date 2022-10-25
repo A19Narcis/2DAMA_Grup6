159f269d2ae46cc6c38eeabcac396ae6eeacfdda
Ins. Ausiàs March v180704 <a19nargomcar@inspedralbes.cat>
2022-10-25 12:12:33 +0200

    Codi Login connexió Server

--- AppMobil/app/src/main/java/com/example/projecte_2dam_grup6/SignIn.java
+++ AppMobil/app/src/main/java/com/example/projecte_2dam_grup6/SignIn.java
@@ -2,10 +2,19 @@ package com.example.projecte_2dam_grup6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SignIn extends AppCompatActivity {
 
@@ -29,4 +38,58 @@ public class SignIn extends AppCompatActivity {
 
     }
 
+    public void validarUserLogin(View view){
+        new MostraTask().execute();
+    }
+
+
+    private class MostraTask extends AsyncTask<String, Void, String>{
+
+        HttpURLConnection con;
+        @Override
+        protected String doInBackground(String... strings) {
+            mostra();
+            return null;
+        }
+
+        private void mostra() {
+            try {
+                URL url = new URL("http://localhost:3000/validarLogIn/" + txtUserSignIn + "/" + txtPasswordSignIn);
+                con = (HttpURLConnection) url.openConnection();
+                con.connect();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String linea;
+                while ((linea = reader.readLine()) != null){
+                    Log.d("Mostra", linea);
+                }
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
